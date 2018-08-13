3f217d734d7b11ec866ab67412a236fc9ce706fe
WANG YULONG <16003749@rp.edu.sg>
2018-08-13 14:45:36 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/anotherreceiverapp/MainActivity.java
@@ -0,0 +1,32 @@
+package sg.edu.rp.c346.anotherreceiverapp;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    BroadcastReceiver br = new AnotherBroadcastReceiver();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction("com.example.broadcast.MY_BROADCAST");
+        this.registerReceiver(br,filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.unregisterReceiver(br);
+    }
+
+
+}
